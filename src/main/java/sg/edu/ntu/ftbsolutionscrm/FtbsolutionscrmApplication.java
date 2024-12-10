472945008d4c813c8479208419fa03b70ba07b84
KhairulAnuwar <khairulanuwarmohamedali86@gmail.com>
2024-12-10 15:25:16 +0800

    Initial commit of Spring Boot project

---
+++ src/main/java/sg/edu/ntu/ftbsolutionscrm/FtbsolutionscrmApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.ftbsolutionscrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtbsolutionscrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtbsolutionscrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/ntu/ftbsolutionscrm/FtbsolutionscrmApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.ftbsolutionscrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtbsolutionscrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
